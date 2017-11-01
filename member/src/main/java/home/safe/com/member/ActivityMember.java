e5b1802d17786b7dbf15e1f6d36945463076d384
Ganapolie <cg072@naver.com>
2017-11-01 15:48:42 +0900

    2017.11.01 ActivityMember 모듈 추가

---
+++ member/src/main/java/home/safe/com/member/ActivityMember.java
@@ -0,0 +1,13 @@
+package home.safe.com.member;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityMember extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member);
+    }
+}
