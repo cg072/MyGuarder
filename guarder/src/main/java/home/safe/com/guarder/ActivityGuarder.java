222260e20ecfdb11732fce4e6e712401f828099a
Ganapolie <cg072@naver.com>
2017-11-01 16:11:40 +0900

    2017.11.01 나머지 모든 모듈 푸시

---
+++ guarder/src/main/java/home/safe/com/guarder/ActivityGuarder.java
@@ -0,0 +1,13 @@
+package home.safe.com.guarder;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityGuarder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guarder);
+    }
+}
---
+++ location/src/main/java/home/safe/com/location/ActivityLocation.java
@@ -0,0 +1,13 @@
+package home.safe.com.location;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityLocation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+    }
+}
