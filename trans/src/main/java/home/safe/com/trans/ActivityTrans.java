012b913f6f82577fc22e68bc4f6994a0f845bae8
plupin724 <plupin724@gmail.com>
2017-11-03 17:24:49 +0900

    2017. 11. 03 박준규 이동수단관리 레이아웃

--- trans/src/main/java/home/safe/com/trans/ActivityTrans.java
+++ trans/src/main/java/home/safe/com/trans/ActivityTrans.java
@@ -2,12 +2,25 @@ package home.safe.com.trans;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+//2017. 11. 03
+//author 박준규
+//이동수단 설정 -> 메인
+//이동수단의 내역 버튼클릭(이동수단 내역에 대한 새로운 액티비티 띄움)
+//이동수단 등록 -> 컨텍스트메뉴(menu_transelect.xml) 이동
+//메모입력
 
 public class ActivityTrans extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_trans);
+
+
+
     }
+
 }
