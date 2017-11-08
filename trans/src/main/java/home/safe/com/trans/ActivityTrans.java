4c7d7eb7826c7f6fb80c4de9721eeda7a9d2e5c6
plupin724 <plupin724@gmail.com>
2017-11-08 13:20:20 +0900

    2017. 11. 08 박준규 이동수단관리 레이아웃 구현

--- trans/src/main/java/home/safe/com/trans/ActivityTrans.java
+++ trans/src/main/java/home/safe/com/trans/ActivityTrans.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.PopupMenu;
 import android.view.ContextMenu;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -15,15 +16,17 @@ import android.widget.Toast;
     author 박준규
     이동수단 설정 -> 메인
     이동수단의 내역 버튼클릭(이동수단 내역에 대한 새로운 액티비티 띄움)
-    이동수단 등록 -> 컨텍스트메뉴(menu_transelect.xml) 이동
-    메모입력*/
+    이동수단 등록 -> 팝업메뉴(menu_transelect.xml) 띄움
+    부가정보 입력 -> 부가정보 내용 저장
+    확인버튼 클릭 -> 메인액티비티로 이동
+    */
 
-public class ActivityTrans extends AppCompatActivity implements View.OnClickListener{
+public class ActivityTrans extends AppCompatActivity implements View.OnClickListener {
 
     //리스트, 선택 영역의 텍스트뷰 선언
     TextView transList;
     TextView transSelect;
-
+    Button btnOk;
 
 
     @Override
@@ -34,6 +37,7 @@ public class ActivityTrans extends AppCompatActivity implements View.OnClickList
         //리스트, 선택 영역의 아이디를 가져오기
         transList = (TextView) findViewById(R.id.transList);
         transSelect = (TextView) findViewById(R.id.transSelect);
+        btnOk = (Button) findViewById(R.id.btnOk);
 
 
         //리스트, 선택 영역에 온클릭리스너 세팅
@@ -44,31 +48,58 @@ public class ActivityTrans extends AppCompatActivity implements View.OnClickList
     }
 
 
-
     //클릭 이벤트
     @Override
     public void onClick(View view) {
         //리스트버튼 클릭
-        if(view == transList){
+        if (view == transList) {
             Intent intentPopup = new Intent(this, ActivityTransListPopup.class);
             startActivity(intentPopup);
         }
 
 
-
-
-        ///////////todo this
         //셀렉트버튼 클릭
-       /* if(view == transSelect){
-            PopupMenu transSelectPopup = new PopupMenu(this, view);
-            MenuInflater inflater = transSelectPopup.getMenuInflater();
-            Menu menu = transSelectPopup.getMenu();
+        if (view == transSelect) {
+            //팝업 메뉴 만들어주기
+            PopupMenu popupTransSelect = new PopupMenu(this, view);
+            //팝
+            MenuInflater inflater = popupTransSelect.getMenuInflater();
+            Menu menu = popupTransSelect.getMenu();
             inflater.inflate(R.menu.menu_transselect, menu);
+            popupTransSelect.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+                @Override
+                public boolean onMenuItemClick(MenuItem item) {
+                    switch (item.getItemId()) {
+                        case R.id.transWalk:
+                            Toast.makeText(getApplicationContext(), "도보", Toast.LENGTH_LONG).show();
+                    }
+                    switch (item.getItemId()) {
+                        case R.id.transBus:
+                            Toast.makeText(getApplicationContext(), "버스", Toast.LENGTH_LONG).show();
+                    }
+                    switch (item.getItemId()) {
+                        case R.id.transTaxi:
+                            Toast.makeText(getApplicationContext(), "택시", Toast.LENGTH_LONG).show();
+                    }
+                    switch (item.getItemId()) {
+                        case R.id.transSub:
+                            Toast.makeText(getApplicationContext(), "지하철", Toast.LENGTH_LONG).show();
+                    }
+                    switch (item.getItemId()) {
+                        case R.id.transEtc:
+                            Toast.makeText(getApplicationContext(), "기타", Toast.LENGTH_LONG).show();
+                    }
 
-        }*/
+                    return false;
+                }
+            });
+            popupTransSelect.show();
+
+
+        }
+
 
     }
-
 
 
 }
