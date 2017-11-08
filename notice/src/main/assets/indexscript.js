738448c5dd797deab7a94770ad744581ec2df94b
plupin724 <plupin724@gmail.com>
2017-11-08 17:12:35 +0900

    2017. 11. 03 박준규 이동수단관리 레이아웃 제이쿼리 파일 추가

--- notice/src/main/assets/indexscript.js
+++ notice/src/main/assets/indexscript.js
@@ -1,4 +1,4 @@
+
 $(document).ready(function() {
-    $("#aaa").append("<div id='myid' title='Hello world?'>hello world</div>");
-    $("#foo").append("<a href='https://www.google.co.kr' title='나를 클릭하면 구글로 간다.'>클릭!</a>");
+    $("#wrap").append("<div id='myid' title='Hello world?'>hello world</div>");
 });
