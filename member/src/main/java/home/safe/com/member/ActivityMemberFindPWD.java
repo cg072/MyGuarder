00bcd95e7a032c5073c3a38e3fdf1ac30a01b4a9
cooljjong <joakim1120@naver.com>
2017-12-05 17:17:10 +0900

    [김종하]
    1. Guarder
       1) 서버 연결전, 필요한 기능들 체크 및 기능 방향 확인
    
    2. Member
       1) 서버 연결전, 필요한 기능들 체크 및 기능 방향 확인

--- member/src/main/java/home/safe/com/member/ActivityMemberFindPWD.java
+++ member/src/main/java/home/safe/com/member/ActivityMemberFindPWD.java
@@ -25,25 +25,5 @@ public class ActivityMemberFindPWD extends AppCompatActivity {
         etID = (EditText)findViewById(R.id.etID);
         etEmail = (EditText)findViewById(R.id.etEmail);
         btnSend = (Button)findViewById(R.id.btnSend);
-
-        btnSend.setOnClickListener(new Button.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                GMailSender sender = new GMailSender(email, password);
-
-                try {
-                    sender.sendMail("타이틀",      // 제목
-                            "내용",          // 내용
-                            "hotkiss86kjh@gmail.com",                 // 보내는이
-                            "joakim1120@naver.com" // 받는이
-                    );
-                    Log.v("이메일", "보내짐");
-                } catch (Exception e) {
-                    Log.v("이메일", "안보내짐");
-                }
-            }
-        });
-
-
     }
 }
